7ce66c0f45f3c5c157e1260492e836b39e1e8383
Veirt <vveirt@gmail.com>
2022-01-25 22:29:10 +0800

    refactor: add filter to `dnpak-find` and upgrade dnpak to 2.0.1

--- src/bin/dnpak-find.rs
+++ src/bin/dnpak-find.rs
@@ -11,17 +11,25 @@ fn main() {
         exit(1);
     });
     let input_path = Path::new(&input).display().to_string();
-
-    // add pak if it's not specified.
     if !input_path.ends_with(".pak") {
         eprintln!("File must be ended with .pak");
         exit(1);
     }
 
     let pak = EtFileSystem::read(&input_path);
-
-    let file_list = pak.get_files();
-    for file in file_list {
-        println!("{}", file);
+    let query = args.next();
+    match query {
+        Some(q) => {
+            let file_list = pak.find_files(&q);
+            for file in file_list {
+                println!("{}", file);
+            }
+        }
+        None => {
+            let file_list = pak.get_files();
+            for file in file_list {
+                println!("{}", file);
+            }
+        }
     }
 }
